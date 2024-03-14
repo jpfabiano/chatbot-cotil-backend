96ce396833471f0cb6bf263e8f50a829b3c5bbf7
João <jpfabiano08@gmail.com>
2024-03-14 20:39:19 -0300

    initial commit

---
+++ src/test/java/com/jpfabiano/botcotil/BotCotilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpfabiano.botcotil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotCotilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

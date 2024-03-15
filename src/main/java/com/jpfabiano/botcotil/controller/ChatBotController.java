4ab859baf607b090806574fcb73d26aba72ad785
aluno <cl203520@g.unicamp.br>
2024-03-15 11:22:45 -0300

    Controller configurado

--- src/main/java/com/jpfabiano/botcotil/controller/ChatBotController.java
+++ src/main/java/com/jpfabiano/botcotil/controller/ChatBotController.java
@@ -1,20 +1,26 @@
 package com.jpfabiano.botcotil.controller;
 
 import com.jpfabiano.botcotil.dto.MessageRequest;
-import org.aspectj.bridge.Message;
+import com.jpfabiano.botcotil.dto.MessageResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.jpfabiano.botcotil.service.FaqService;
 
 @RestController
 @RequestMapping("/chat")
 public class ChatBotController {
+    @Autowired
+    private FaqService faqService;
+
     @PostMapping
-    public ResponseEntity<String> question (@RequestBody MessageRequest messageRequest){
-        System.out.println(messageRequest.message());
-        return ResponseEntity.status(HttpStatus.OK).body("Deu certo!");
+    public ResponseEntity<MessageResponse> question (@RequestBody MessageRequest messageRequest){
+        String ansewr = faqService.getAnswer(messageRequest.message());
+        MessageResponse response = new MessageResponse(ansewr);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
